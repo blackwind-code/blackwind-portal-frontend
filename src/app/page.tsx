a2b14bec6102d6def7c85cfe7395696e2a38a8c8
baek <baekhh123@gmail.com>
2025-05-10 14:13:41 +0900

    add word 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,14 @@ const HomePage: NextPage = () => {
         <div className="w-full bg-[#1F3C70] text-white py-10 px-6 md:px-20">
           <div className="text-left space-y-4">
             <h2 className="text-xl font-bold">Blackwind</h2>
-              <div className="text-sm leading-relaxed">
+            <div className="text-sm leading-relaxed">
+              대구광역시 달성군 유가읍 테크노중앙대로 333 E10 B131호<br />
+              <a
+                href="mailto:blackwind@dgist.ac.kr"
+                className="underline hover:text-gray-300"
+              >
+                blackwind@dgist.ac.kr
+                </a>
             </div>
         </div>
       </div>
