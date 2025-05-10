468495a709079a4d5b136c7904e3cd0b12bb4e09
baek <baekhh123@gmail.com>
2025-05-10 17:54:49 +0900

    'more' button added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,6 +91,13 @@ const HomePage: NextPage = () => {
                 </div>
               ))}
           </div>
+
+          {/* 더보기 버튼 */}
+          <div className="flex justify-end">
+            <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+              더보기
+            </button>
+          </div>
           </div>
         </section>
         <section className="h-screen snap-start bg-gray-300 text-black p-10 pt-[64px]">
